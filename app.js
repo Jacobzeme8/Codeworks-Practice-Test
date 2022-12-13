db0f117abaeccc7996e585963a3c7729922e00cb
Jacob <Jacobzeme8@gmail.com>
2022-12-12 22:08:47 -0700

    done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 18
+const ageToVote = 100
 
 const user = {
   name: 'Sherlock Holmes',
@@ -17,5 +17,45 @@ const cases = [
 ]
 
 // YOUR JS CODE
+
+let currentYear = 2022
+let jsReleaseYear = 1995
+
+  let age = currentYear - jsReleaseYear
+  console.log("Current age of java script is ", age)
+
+
+
+if(age >= ageToVote){
+  console.log("JavaScript is old enough to vote")
+}
+else{
+  console.log("JavaScript is not old enought to vote")
+}
+console.log("starting")
+let i
+for(i=0; i<=100; i++){
+  if(i % 10 ==0 ){
+  console.log(i)
+  }
+  if(i==50){
+    console.log("Halfway")
+  }
+  if(i==100){
+    console.log("finished")
+  }
+}
+
+
+console.log(`${user.name} lives at ${user.address.street} ${user.address.city}`)
+
+for(let x=0; x<5; x++){
+  if(cases[x].releaseYear == 1903){
+  console.log(cases[x].title, cases[x].releaseYear)
+  }
+}
+
+
+
 
 
